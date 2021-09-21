aba81a44070430b3371b1392cd2ef7ded0a02ec4
Abdulaziz <abdulaziztubah@gmail.com>
2021-09-21 15:37:21 +0200

    First commit

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    // represent database in Code Based
+    public class SchoolContext : DbContext
+    {
+        // open connection with database
+        public SchoolContext():base("SchoolContext")
+        {
+        }
+
+        // represent tables in Code Based
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // remave plural names in table names
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // pure join table
+            modelBuilder.Entity<Course>()
+             .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+             .Map(t => t.MapLeftKey("CourseID")
+                 .MapRightKey("InstructorID")
+                 .ToTable("CourseInstructor"));
+        }
+    }
+}
